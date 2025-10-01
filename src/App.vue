827187201cb156dfa578a1479d7fa0c4f2220f59
Santiagocapi <capitanisantiago@gmail.com>
2025-10-01 19:56:31 -0300

    Starting the portfolio, we create the component for the navigation bar

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import NavBar from './components/Navbar.vue'
+</script>
+
+<template>
+  <!-- Contenido de html con herramientas de Vue -->
+  <header>
+    <NavBar />
+  </header>
+</template>
+
+<style scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar">
+    <h2>Portafolio Personal</h2>
+
+    <div class="navegation">
+      <a v-for="item in navegation" :key="item.id" :href="item.link">{{ item.name }}</a>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue' // Importar ref para crear referencias reactivas
+const navegation = ref([
+  { id: 1, name: 'Educacion', link: '#education' },
+  { id: 2, name: 'Proyectos', link: '#project' },
+  { id: 3, name: 'Contacto', link: '#contact' },
+])
+</script>
+
+<style scoped>
+* {
+  text-decoration: none;
+}
+</style>
